3394d7e67cdcd0b3b17c98b7f66fe2ac034b9719
Mehdi <m.lloyal93@gmail.com>
2018-12-19 09:32:00 -0500

    Updated some reveal element.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,18 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
-// var revealOnScroll = new RevealOnScroll();
-new RevealOnScroll($(".large-hero__text-content"), "10%");
-new RevealOnScroll($(".site-header__logo"), "-500px");
+// toggle reveals of elements below
+new RevealOnScroll($(".large-hero__text-content"), "11rem");
+new RevealOnScroll($(".site-header__logo"), "-670px");
+
+
+// header color change below
+new StickyHeader($(".large-hero__title"), "site-header--dark");
+new StickyHeader($(".page-section__trigger--first"), "site-header--dark1");
+new StickyHeader($(".page-section__trigger--second"), "site-header--dark2");
+var stickyHeader = new StickyHeader();
+
+// 3 errors atm for fav icon
